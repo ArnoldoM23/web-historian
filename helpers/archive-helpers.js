38460ce3324bca548a471c7a94ee81b6266c3d16
ArnoldoM23 <arnoldomunoz23@yahoo.com>
2016-07-13 15:46:06 -0700

    start web historian

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,9 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  index: path.join(__dirname, '../web/public/index.html'),
+
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -26,9 +28,11 @@ exports.initialize = function(pathsObj){
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(){
+
 };
 
 exports.isUrlInList = function(){
+
 };
 
 exports.addUrlToList = function(){
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,38 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var request = require('request');
+var head = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
+ // var readF = fs.writeFile(archive.paths.index)
+ console.log('read************!',fs.readFile(archive.paths.index));
+// console.log(path.extname('index.html'));
+console.log('arc*********',archive.paths.index);
+// console.log(path.archives);
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+ var statusCode = 200;
+ var sendUrl = '';
+
+
+fs.readFile(archive.paths.index, function(err, data){
+  if (err) {
+    throw err;
+  }
+  var raw = data.toString();
+  console.log('****************************raw',raw.indexOf('<input'));
+});
+
+// var input = raw.indexOf('<index');
+
+
+ if (req.url === '/') {
+  sendUrl = archives.paths.index;
+ }else if(req.url === '/www.google.com'){
+  sendUrl = '/google/';
+ }
+
+  // res.writeHead(statusCode, head.headers); 
+  // res.end(archive.paths.list);
+  res.end(sendUrl);
 };
